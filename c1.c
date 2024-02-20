d0a204233f9e4f10057652498ec521b00274554c
zhaoyanjie001 <8324750+zhaoyanjie001@users.noreply.github.com>
2024-02-20 01:47:53 +0000

    single link

--- c1.c
+++ c1.c
@@ -13,13 +13,20 @@ int main(void)
 {
 	list_single *node = NULL ;          //1、首先，当然是定义一个头指针
 	node = (list_single *)malloc(sizeof(list_single)); //2、然后分配内存空间
+	list_single *node1 = NULL ;          //1、首先，当然是定义一个头指针
+	node1 = (list_single *)malloc(sizeof(list_single)); //2、然后分配内存空间
 	if(node == NULL){
 		printf("malloc fair!\n");
 	}
 	memset(node,0,sizeof(list_single)); //3、清一下
 	node->data = 100 ;		    		//4、给链表节点的数据赋值
-	node->next = NULL ;                 //5、将链表的指针域指向空
+	       
+	memset(node1,0,sizeof(list_single)); //3、清一下
+	node1->data = 101 ;		    		//4、给链表节点的数据赋值
+	node->next = node1;     
+	node1->next = NULL ;                 //5、将链表的指针域指向空
 	printf("%d\n",node->data);
+	printf("%d\n",node->next->data);
 	free(node);
 	return 0 ;
 }
